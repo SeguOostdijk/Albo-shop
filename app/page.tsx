ca55b8455b76281a64ae3cfa81c18858207e5aa0
Bautista <Bautistanielsen2003@hotmail.com>
2026-03-13 19:18:36 -0300

    as

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default async function HomePage() {
       {/* Featured Products */}
       <ProductCarousel
         title="Destacado"
-        subtitle="PRODUCTOS DESTACADOS"
+        subtitle="PRODUCTOS DESTACADO"
         products={featuredProducts}
         viewAllHref="/category/novedades"
       />
